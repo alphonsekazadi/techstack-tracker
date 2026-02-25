b05e80ee8512d80767c3eae5fdabf1552eeb575e
alphonsekazadi <alphonsekazadi01@gmail.com>
2026-02-25 09:50:29 +0200

    Add home route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 // This is web routes
 Route::get('/', [TechnoController::class, 'index'])->name('techno.index');
 Route::post('/technos', [TechnoController::class, 'store'])->name('techno.store');
+
+Route::get('/home', function () {
+    return view('home');
+});
