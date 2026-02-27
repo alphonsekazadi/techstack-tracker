a0a7f384d7fcf16c7af2cb01adfc2c946d783e34
alphonsekazadi <alphonsekazadi01@gmail.com>
2026-02-27 18:41:02 +0200

    Add routes for technologies and progress pages

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use App\Http\Controllers\TechnoController;
 use Illuminate\Support\Facades\Route;
 
-// This is web routes
+// Dashboard
+Route::get('/', [TechnoController::class, 'index'])->name('home');
+
+// Technologies
+Route::get('/technologies', [TechnoController::class, 'technologies'])->name('technologies');
+
+// Progress
+Route::get('/progress', [TechnoController::class, 'progress'])->name('progress');
+
+// CRUD routes
 Route::resource('technos', TechnoController::class);
 Route::patch('technos/{techno}/toggle', [TechnoController::class, 'toggleMaitrise'])->name('technos.toggle');
