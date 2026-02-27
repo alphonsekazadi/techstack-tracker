38b94240545fa0dfa0bcb54ee0430ac6a9290cf7
alphonsekazadi <alphonsekazadi01@gmail.com>
2026-02-27 06:31:52 +0200

    delete home page

--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Home - TechStack Tracker')
-
-@section('content')
-    <h1 class="text-3xl font-bold text-gray-800">Welcome to TechStack Tracker</h1>
-@endsection
