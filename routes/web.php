05911d3f3cbe6ab07c933a95061d91dc0dd38b36
alphonsekazadi <alphonsekazadi01@gmail.com>
2026-02-27 07:54:58 +0200

    Add some comments

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\TechnoController;
 use Illuminate\Support\Facades\Route;
 
 // This is web routes
+
+// Get techno route
 Route::get('/', [TechnoController::class, 'index'])->name('techno.index');
+
+// Post techno route
 Route::post('/technos', [TechnoController::class, 'store'])->name('techno.store');
 
+// Delete route
 Route::delete('/techno/{techno}', [TechnoController::class, 'destroy'])->name('techno.destroy');
